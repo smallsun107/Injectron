06fb199f51aba51a26a92390177680e6b1be219e
smallsun <smallsun107@gmail.com>
2025-05-08 23:19:20 +0800

    fix: add alert

--- fucker/fucker/fucker.m
+++ fucker/fucker/fucker.m
@@ -25,33 +25,31 @@ int fakeHasValidOfflineReplyCode(void* _this) {
     const char *moduleName = _dyld_get_image_name(0);
     NSLog(@"[-] moduleName: %s",moduleName);
     if (strstr(moduleName, "Understand") != NULL){
+        [self showAlert];
         intptr_t addr = _dyld_get_image_vmaddr_slide(0) + 0x17A3F50 + 0x100000000;
-        dispatch_async(dispatch_get_main_queue(), ^{
-               NSAlert *alert = [[NSAlert alloc] init];
-               [alert setMessageText:@"✅"];
-               [alert setInformativeText:@"Cracked by sma11sun"];
-               [alert addButtonWithTitle:@"OK"];
-               [alert runModal];
-        });
         NSLog(@"[-] Understand Target address: %p", (void*)addr);
     //    tiny_hook((void*)addr, fakeHasValidOfflineReplyCode, (void*)&original);
         DobbyHook((void*)addr, fakeHasValidOfflineReplyCode, (void*)&original);
     }
     if (strstr(moduleName, "BCompare") != NULL) {
+        [self showAlert];
         intptr_t addr = _dyld_get_image_vmaddr_slide(0) + 0x1027B31 + 0x100000000;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            NSAlert *alert = [[NSAlert alloc] init];
-            [alert setMessageText:@"✅"];
-            [alert setInformativeText:@"Cracked by sma11sun"];
-            [alert addButtonWithTitle:@"OK"];
-            [alert runModal];
-        });
         NSLog(@"[-] BCompare Target address: %p", (void*)addr);
         uint8_t data[] = {'n'};
         DobbyCodePatch((void*)addr, data, sizeof(data));
     }
     
     
+}
+
++ (void) showAlert{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setMessageText:@"✅"];
+        [alert setInformativeText:@"Cracked by sma11sun"];
+        [alert addButtonWithTitle:@"OK"];
+        [alert runModal];
+    });
 }
 
 @end
